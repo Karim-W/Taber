e0f1efe8bb08a31892c828e1eef9710f0b5f30d6
Karim <karim.wael@gmail.com>
2022-06-15 23:56:47 +0400

    edge to edge display

---
+++ taber/Pages/MainUIView.swift
@@ -0,0 +1,20 @@
+//
+//  MainUIView.swift
+//  taber
+//
+//  Created by Karim Hassan on 15/06/2022.
+//
+
+import SwiftUI
+
+struct MainUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainUIView()
+    }
+}
