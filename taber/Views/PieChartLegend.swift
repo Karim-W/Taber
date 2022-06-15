dcb890c4dfae2c2c3b385d865c4b00f45fe342bd
Karim <karim.wael@gmail.com>
2022-06-15 20:21:17 +0400

    content view updates

---
+++ taber/Views/PieChartLegend.swift
@@ -0,0 +1,20 @@
+//
+//  PieChartLegend.swift
+//  taber
+//
+//  Created by Karim Hassan on 15/06/2022.
+//
+
+import SwiftUI
+
+struct PieChartLegend: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PieChartLegend_Previews: PreviewProvider {
+    static var previews: some View {
+        PieChartLegend()
+    }
+}
